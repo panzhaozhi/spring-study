5bef2dedaf35e4c773a42d67b80866b14232590d
panzz <panzz@ushareit.com>
2020-06-23 21:41:58 +0800

    add junit test and file read

---
+++ src/test/java/org/study/junit/Test5.java
@@ -0,0 +1,17 @@
+package org.study.junit;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class Test5 {
+
+    @Test
+    @DisplayName("exception test")
+    public void exceptionTest(){
+        int sum = 1000;
+        if(sum%1000 == 0){
+            System.out.println(sum);
+        }
+    }
+
+}
